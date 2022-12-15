9ea4c305c40ea8ee9b745a83d934a4f0d8984b05
Bismarck da Silva Guimarães <119543603+BismarckRudvan@users.noreply.github.com>
2022-12-15 19:54:14 -0300

    Atualização

--- Martingale.js
+++ Martingale.js
@@ -2,30 +2,42 @@ function martingale(){
     let a = document.getElementById('inum')
     let numero = Number(a.value)
     let max = document.getElementById('imax')
+    let odd = document.getElementById('odd')
+    let nodd = Number(odd.value)
+    let perda = -numero
+    let meta = numero*nodd
+    let apostado = 0
+    let cont = numero
     let contgale = 0
+    let contgale2 = 0
+    let banca = numero
     let etapa = []
     let numgale = Number(max.value)
-    let cont = numero
     let res = document.getElementById('res')
 
-    if(a.value.length == 0 || max.value.length == 0){
-        window.alert ('POR FAVOR, PREENCHA TODOS OS CAMPOS!')
+    if(a.value.length == 0 || max.value.length == 0 || odd.value.length == 0){
+        window.alert (`POR FAVOR, PREENCHA TODOS OS CAMPOS!${perda}`)
     }else{
         while(contgale < numgale){
-            numero += numero
-            cont += numero
-            etapa.push(numero)
+            while(perda < meta){
+                cont += cont*nodd - cont
+                perda = cont
+                contgale2++
+            }
+            perda = -numero
+            perda -= -cont*contgale2
+            etapa.push(perda) 
             contgale++
         }
-        let vitoria = cont + Number(a.value)
+        let vitoria = banca + Number(a.value)
 
         if(document.getElementsByName('moeda')[0].checked){
-            res.innerHTML = `Você precisa ter <strong>${cont.toLocaleString('pt-br' , {style: 'currency' , currency: 'brl'})}</strong> para fazer este Martingale! <br><br><br> Sua aposta iniciará em <strong>${Number(a.value).toLocaleString('pt-br' , {style: 'currency' , currency: 'brl'})}</strong>, e seu Martingale seguirá as seguintes etapas abaixo: <br><br>(<strong>${etapa.toLocaleString('pt-br' , {style: 'currency' , currency: 'brl'})}</strong>)<br><br><br> E em caso de vitória, você ficará com um valor total de <strong>${vitoria.toLocaleString('pt-br' , {style:'currency' , currency: 'brl'})}</strong>!`}
+            res.innerHTML = `Você precisa ter <strong>${banca.toLocaleString('pt-br' , {style: 'currency' , currency: 'brl'})}</strong> para fazer este Martingale! <br><br><br> Sua aposta iniciará em <strong>${Number(a.value).toLocaleString('pt-br' , {style: 'currency' , currency: 'brl'})}</strong>, e seu Martingale seguirá as seguintes etapas abaixo: <br><br>(<strong>${etapa.toLocaleString('pt-br' , {style: 'currency' , currency: 'brl'})}</strong>)<br><br><br> E em caso de vitória, você ficará com um valor total de <strong>${vitoria.toLocaleString('pt-br' , {style:'currency' , currency: 'brl'})}</strong>!`}
 
             else if(document.getElementsByName('moeda')[1].checked){
-                res.innerHTML = `Você precisa ter <strong>${cont.toLocaleString('pt-br' , {style: 'currency' , currency: 'usd'})}</strong> para fazer este Martingale! <br><br><br> Sua aposta iniciará em <strong>${Number(a.value).toLocaleString('pt-br' , {style: 'currency' , currency: 'usd'})}</strong>, e seu Martingale seguirá as seguintes etapas abaixo: <br><br>(<strong>${etapa.toLocaleString('pt-br' , {style: 'currency' , currency: 'usd'})}</strong>)<br><br><br> E em caso de vitória, você ficará com um valor total de <strong>${vitoria.toLocaleString('pt-br' , {style:'currency' , currency: 'usd'})}</strong>!`}
+                res.innerHTML = `Você precisa ter <strong>${banca.toLocaleString('pt-br' , {style: 'currency' , currency: 'usd'})}</strong> para fazer este Martingale! <br><br><br> Sua aposta iniciará em <strong>${Number(a.value).toLocaleString('pt-br' , {style: 'currency' , currency: 'usd'})}</strong>, e seu Martingale seguirá as seguintes etapas abaixo: <br><br>(<strong>${etapa.toLocaleString('pt-br' , {style: 'currency' , currency: 'usd'})}</strong>)<br><br><br> E em caso de vitória, você ficará com um valor total de <strong>${vitoria.toLocaleString('pt-br' , {style:'currency' , currency: 'usd'})}</strong>!`}
 
                 else if (document.getElementsByName('moeda')[2].checked){
-                    res.innerHTML = `Você precisa ter <strong>${cont.toLocaleString('pt-br' , {style: 'currency' , currency: 'eur'})}</strong> para fazer este Martingale! <br><br><br> Sua aposta iniciará em <strong>${Number(a.value).toLocaleString('pt-br' , {style: 'currency' , currency: 'eur'})}</strong>, e seu Martingale seguirá as seguintes etapas abaixo: <br><br>(<strong>${etapa.toLocaleString('pt-br' , {style: 'currency' , currency: 'eur'})}</strong>)<br><br><br> E em caso de vitória, você ficará com um valor total de <strong>${vitoria.toLocaleString('pt-br' , {style:'currency' , currency: 'eur'})}</strong>!`}
+                    res.innerHTML = `Você precisa ter <strong>${banca.toLocaleString('pt-br' , {style: 'currency' , currency: 'eur'})}</strong> para fazer este Martingale! <br><br><br> Sua aposta iniciará em <strong>${Number(a.value).toLocaleString('pt-br' , {style: 'currency' , currency: 'eur'})}</strong>, e seu Martingale seguirá as seguintes etapas abaixo: <br><br>(<strong>${etapa.toLocaleString('pt-br' , {style: 'currency' , currency: 'eur'})}</strong>)<br><br><br> E em caso de vitória, você ficará com um valor total de <strong>${vitoria.toLocaleString('pt-br' , {style:'currency' , currency: 'eur'})}</strong>!`}
     }
 }
